20e1335a6292e6063f058bb009adcb7b7b250db6
Sami Kukkonen <sami@samikukkonen.fi>
2018-12-22 15:44:36 +0200

    DRY dummy data deserialization functions

--- src/containers/DummyCourseList.tsx
+++ src/containers/DummyCourseList.tsx
@@ -1,40 +1,8 @@
 import React from 'react'
-import moment from 'moment'
 import CourseList from '../components/CourseList'
 import courses from '../data/courses.json'
-import { Document, Course } from '../domain'
+import { deserialize } from './deserializer'
 
-// #region Domain type representations before deserialization
-interface DocumentRepr {
-  filename: string
-  lastModified?: string
-  size?: string
-}
-
-interface CourseRepr {
-  name: string
-  documents?: Array<DocumentRepr>
-}
-// #endregion
-
-const momentifyLastModified = (document: DocumentRepr): Document => {
-  return {
-    ...document,
-    lastModified: moment(document.lastModified)
-  }
-}
-
-const apiToAppModel = (courses: Array<CourseRepr>): Array<Course> => {
-  const appModelCourses = courses.map(course => ({
-    ...course,
-    documents: course.documents
-      ? course.documents.map(momentifyLastModified)
-      : undefined
-  }))
-  appModelCourses.sort((a, b) => a.name.localeCompare(b.name))
-  return appModelCourses
-}
-
-const DummyCourseList = () => <CourseList courses={apiToAppModel(courses)} />
+const DummyCourseList = () => <CourseList courses={deserialize(courses)} />
 
 export default DummyCourseList
--- src/containers/DummyDocumentList.tsx
+++ src/containers/DummyDocumentList.tsx
@@ -1,47 +1,15 @@
 import React from 'react'
-import moment from 'moment'
 import dummyCourses from '../data/courses.json'
 import DocumentList from '../components/DocumentList'
 import NotFound from '../components/NotFound'
-import { Course, Document } from '../domain'
-
-// #region Domain type representations before deserialization
-interface DocumentRepr {
-  filename: string
-  lastModified?: string
-  size?: string
-}
-
-interface CourseRepr {
-  name: string
-  documents?: Array<DocumentRepr>
-}
-// #endregion
-
-const momentifyLastModified = (document: DocumentRepr): Document => {
-  return {
-    ...document,
-    lastModified: moment(document.lastModified)
-  }
-}
-
-const apiToAppModel = (courses: Array<CourseRepr>): Array<Course> => {
-  const appModelCourses = courses.map(course => ({
-    ...course,
-    documents: course.documents
-      ? course.documents.map(momentifyLastModified)
-      : undefined
-  }))
-  appModelCourses.sort((a, b) => a.name.localeCompare(b.name))
-  return appModelCourses
-}
+import { deserialize } from './deserializer'
 
 const DummyDocumentList = ({
   match
 }: {
   match: { params: { courseName: string } }
 }) => {
-  const courses = apiToAppModel(dummyCourses)
+  const courses = deserialize(dummyCourses)
   const requestedCourse = match.params.courseName
   const matchingCourse = courses.find(course => course.name === requestedCourse)
   if (!matchingCourse) return <NotFound />
