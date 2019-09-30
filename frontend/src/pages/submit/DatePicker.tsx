6a51aefb3cd5729dbbabb030eff4b979b5c5d61c
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-09-30 22:32:20 +0300

    Use Material UI calendar icon for date picker

--- frontend/src/pages/submit/DatePicker.tsx
+++ frontend/src/pages/submit/DatePicker.tsx
@@ -4,11 +4,26 @@ import ReactDatePicker, { DatePickerProps } from 'react-date-picker'
 import { WithClassName } from '../common/WithClassName'
 import './DatePicker.scss'
 
+const CalendarIcon: FunctionComponent<WithClassName> = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="19"
+    height="19"
+    viewBox="0 0 24 24"
+    className={className}
+  >
+    <path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z" />
+    <path fill="none" d="M0 0h24v24H0z" />
+  </svg>
+)
+
 interface Props extends WithClassName {
   value: Date | undefined
   onChange: (date: Date) => void
   disabled?: boolean
 }
+
+const calendarIcon = <CalendarIcon className="date-picker__calendar-icon" />
 
 const DatePicker: FunctionComponent<Props> = ({
   className,
@@ -32,6 +47,7 @@ const DatePicker: FunctionComponent<Props> = ({
       disabled={disabled}
       className={cls('date-picker', className)}
       calendarClassName="date-picker__calendar"
+      calendarIcon={calendarIcon}
       value={value}
       onChange={handleChange}
       format="y-MM-dd"
