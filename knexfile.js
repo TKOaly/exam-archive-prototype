7f0907eb3a0a61d3beaadbacfb3761e6640ca2ac
Joona Heikkilä <joona.o.heikkila@gmail.com>
2020-07-13 21:45:02 +0300

    Reduce conn pool max

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
     connection: PG_CONNECTION_STRING,
     pool: {
       min: 1,
-      max: 5
+      max: 2
     },
     migrations: {
       tableName: 'knex_migrations'
@@ -38,7 +38,7 @@ module.exports = {
     connection: PG_CONNECTION_STRING,
     pool: {
       min: 1,
-      max: 5
+      max: 2
     },
     migrations: {
       tableName: 'knex_migrations'
