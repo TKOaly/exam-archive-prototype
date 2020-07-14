28872c309a2965bcf5e8b4740421daf2d3633914
Joona Heikkilä <joona.o.heikkila@gmail.com>
2020-07-14 17:24:58 +0300

    Don't use signed cookies after all
    signed URLs don't need cookie consent

--- src/download.ts
+++ src/download.ts
@@ -15,6 +15,7 @@ const oneHourToSeconds = 60 * 60 * 1
 
 const getCloudFrontUrl = (exam: DbExam) =>
   `https://${config.AWS_CF_DISTRIBUTION_DOMAIN}/${exam.file_path}`
+
 const getCfSigningOptions = (
   exam: DbExam
 ): AWS.CloudFront.Signer.SignerOptionsWithoutPolicy => ({
@@ -28,35 +29,6 @@ const createSignedUrl = (exam: DbExam) =>
       err ? reject(err) : resolve(url)
     )
   })
-
-const createSignedCookie = (exam: DbExam) =>
-  new Promise<AWS.CloudFront.Signer.CannedPolicy>((resolve, reject) => {
-    cfSigner.getSignedCookie(getCfSigningOptions(exam), (err, cookieOpts) =>
-      err ? reject(err) : resolve(cookieOpts)
-    )
-  })
-
-const applySignedCookie = async (exam: DbExam, res: express.Response) => {
-  const cookie = await createSignedCookie(exam)
-  const setCookie = (key: string, value: string | number) => {
-    res.cookie(key, value, {
-      domain: '.tko-aly.fi',
-      maxAge: new Date().getTime() + oneHourToSeconds * 1000,
-      httpOnly: true,
-      secure: true,
-      path: '/',
-      sameSite: 'strict'
-    })
-  }
-
-  for (const key of [
-    'CloudFront-Expires',
-    'CloudFront-Key-Pair-Id',
-    'CloudFront-Signature'
-  ] as const) {
-    setCookie(key, cookie[key])
-  }
-}
 
 const router = express()
 
@@ -73,17 +45,8 @@ router.get('/:examId/:fileName', async (req, res, next) => {
     return res.status(404).send('404')
   }
 
-  // only use signed cookies instead of signed urls because
-  // local dev isn't running under the *.tko-aly.fi domain, so
-  // the cookies can't be applied to the correct domain
-  if (config.AWS_CF_USE_SIGNED_COOKIES) {
-    const url = getCloudFrontUrl(exam)
-    await applySignedCookie(exam, res)
-    res.redirect(url)
-  } else {
-    const url = await createSignedUrl(exam)
-    res.redirect(url)
-  }
+  const url = await createSignedUrl(exam)
+  res.redirect(url)
 })
 
 export default router
