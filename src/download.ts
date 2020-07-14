f81d2c20f1983e7cd0d619e946fcaeb06cf1d32d
Joona Heikkilä <joona.o.heikkila@gmail.com>
2020-07-14 17:04:39 +0300

    Add env feature flag for using signed cookies
    Originally we used signed URLs because the test env isn't running under
    *.tko-aly.fi so we couldn't set the cookie for the right domain (the CDN
    domain). In prod, we can, so allow using signed cookies via an environment
    variable.

--- src/download.ts
+++ src/download.ts
@@ -15,23 +15,48 @@ const oneHourToSeconds = 60 * 60 * 1
 
 const getCloudFrontUrl = (exam: DbExam) =>
   `https://${config.AWS_CF_DISTRIBUTION_DOMAIN}/${exam.file_path}`
+const getCfSigningOptions = (
+  exam: DbExam
+): AWS.CloudFront.Signer.SignerOptionsWithoutPolicy => ({
+  expires: Math.floor(new Date().getTime() / 1000) + oneHourToSeconds,
+  url: getCloudFrontUrl(exam)
+})
 
 const createSignedUrl = (exam: DbExam) =>
   new Promise<string>((resolve, reject) => {
-    cfSigner.getSignedUrl(
-      {
-        expires: Math.floor(new Date().getTime() / 1000) + oneHourToSeconds,
-        url: getCloudFrontUrl(exam)
-      },
-      (err, url) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(url)
-        }
-      }
+    cfSigner.getSignedUrl(getCfSigningOptions(exam), (err, url) =>
+      err ? reject(err) : resolve(url)
     )
   })
+
+const createSignedCookie = (exam: DbExam) =>
+  new Promise<AWS.CloudFront.Signer.CannedPolicy>((resolve, reject) => {
+    cfSigner.getSignedCookie(getCfSigningOptions(exam), (err, cookieOpts) =>
+      err ? reject(err) : resolve(cookieOpts)
+    )
+  })
+
+const applySignedCookie = async (exam: DbExam, res: express.Response) => {
+  const cookie = await createSignedCookie(exam)
+  const setCookie = (key: string, value: string | number) => {
+    res.cookie(key, value, {
+      domain: '.tko-aly.fi',
+      maxAge: new Date().getTime() + oneHourToSeconds * 1000,
+      httpOnly: true,
+      secure: true,
+      path: '/',
+      sameSite: 'strict'
+    })
+  }
+
+  for (const key of [
+    'CloudFront-Expires',
+    'CloudFront-Key-Pair-Id',
+    'CloudFront-Signature'
+  ] as const) {
+    setCookie(key, cookie[key])
+  }
+}
 
 const router = express()
 
@@ -48,8 +73,17 @@ router.get('/:examId/:fileName', async (req, res, next) => {
     return res.status(404).send('404')
   }
 
-  const url = await createSignedUrl(exam)
-  return res.redirect(url)
+  // only use signed cookies instead of signed urls because
+  // local dev isn't running under the *.tko-aly.fi domain, so
+  // the cookies can't be applied to the correct domain
+  if (config.AWS_CF_USE_SIGNED_COOKIES) {
+    const url = getCloudFrontUrl(exam)
+    applySignedCookie(exam, res)
+    res.redirect(url)
+  } else {
+    const url = await createSignedUrl(exam)
+    res.redirect(url)
+  }
 })
 
 export default router
