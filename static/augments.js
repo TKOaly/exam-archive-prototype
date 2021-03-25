d2ad97a551b858b7e38233a55d05bf5555e5cc54
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2021-03-26 00:51:56 +0200

    Refactor augments.js a little
    Extract functionality into functions

--- static/augments.js
+++ static/augments.js
@@ -1,52 +1,60 @@
-;[
-  ...document.getElementsByClassName('course-table-row__rename-button')
-].forEach(button => {
-  button.addEventListener('click', e => {
-    e.preventDefault()
-    const { id, currentName } = button.dataset
-    const res = prompt(
-      `Please enter the new name for this course:`,
-      currentName
-    )
-    if (!res) {
-      return
-    }
-
-    const formData = new URLSearchParams()
-    formData.append('name', res)
-
-    return fetch(`/archive/rename-course/${encodeURI(id)}`, {
-      method: 'POST',
-      body: formData,
-      credentials: 'same-origin'
-    })
-      .then(res => {
-        if (res.status === 200) {
-          location.reload()
-          return
-        }
-        if (res.status === 404) {
-          alert('Course not found!')
-          return
-        }
-        alert('An error occurred.')
-      })
-      .catch(e => {
-        console.error(e)
-        alert('An error occurred.')
-      })
-  })
-})
-;[...document.getElementsByClassName('delete-exam-button')].forEach(
-  deleteForm => {
-    const handler = e => {
-      if (!confirm('Are you sure you want to delete this exam?')) {
-        e.preventDefault()
-        return false
+function attachCourseRenameHandlers() {
+  ;[
+    ...document.getElementsByClassName('course-table-row__rename-button')
+  ].forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault()
+      const { id, currentName } = button.dataset
+      const res = prompt(
+        `Please enter the new name for this course:`,
+        currentName
+      )
+      if (!res) {
+        return
       }
 
-      return true
+      const formData = new URLSearchParams()
+      formData.append('name', res)
+
+      return fetch(`/archive/rename-course/${encodeURI(id)}`, {
+        method: 'POST',
+        body: formData,
+        credentials: 'same-origin'
+      })
+        .then(res => {
+          if (res.status === 200) {
+            location.reload()
+            return
+          }
+          if (res.status === 404) {
+            alert('Course not found!')
+            return
+          }
+          alert('An error occurred.')
+        })
+        .catch(e => {
+          console.error(e)
+          alert('An error occurred.')
+        })
+    })
+  })
+}
+
+function attachExamDeleteConfirmationHandlers() {
+  ;[...document.getElementsByClassName('delete-exam-button')].forEach(
+    deleteForm => {
+      const handler = e => {
+        if (!confirm('Are you sure you want to delete this exam?')) {
+          e.preventDefault()
+          return false
+        }
+
+        return true
+      }
+      deleteForm.addEventListener('submit', handler)
     }
-    deleteForm.addEventListener('submit', handler)
-  }
-)
+  )
+}
+
+attachCourseRenameHandlers()
+attachExamDeleteConfirmationHandlers()
